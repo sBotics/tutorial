6b9eb4b7d513f1e45f5c01ce13dc69b20a7c06bc
lluckymou <lluckymou@gmail.com>
2021-03-26 18:22:55 -0300

    Tiles

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,21 @@
-$(document).ready(function(){
+$(document).ready(function() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const language = urlParams.get('lang');
+    
     $( ".hamburger-menu" ).click(function() {
         $( ".hamburger-menu" ).toggleClass( "pressed" );
         $( ".side-navigation" ).toggleClass( "hidden" );
     });
+    $( "a" ).each(function() {
+        var href= $(this).attr("href");
+        if(href[0] != '#')
+            $(this).attr("href", href + "?lang=" + language);
+    });
+    switch(language) {
+        case "pt-br":
+            $( ".EN" ).addClass( "d-none" );
+            break;
+        default:
+            $( ".PT-BR" ).addClass( "d-none" );
+    }
 });
