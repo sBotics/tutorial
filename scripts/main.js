190ec14a22bc576926f1b72187b41efe09752066
lluckymou <lluckymou@gmail.com>
2021-03-27 01:19:49 -0300

    test

--- scripts/main.js
+++ scripts/main.js
@@ -1,21 +1,39 @@
+const urlParams = new URLSearchParams(window.location.search);
+const language = urlParams.get('lang');
+
+// languages
+var PT_BR;
+var EN;
+
+$.getJSON("./pt-br.json", function(json){
+    PT_BR = json;
+});
+$.getJSON("./en.json", function(json){
+    EN = json;
+});
+
 $(document).ready(function() {
-    const urlParams = new URLSearchParams(window.location.search);
-    const language = urlParams.get('lang');
-    
     $( ".hamburger-menu" ).click(function() {
         $( ".hamburger-menu" ).toggleClass( "pressed" );
         $( ".side-navigation" ).toggleClass( "hidden" );
     });
+    
+    // propagates the chosen language to other anchors in the page
     $( "a" ).each(function() {
         var href= $(this).attr("href");
         if(href[0] != '#')
             $(this).attr("href", href + "?lang=" + language);
     });
-    switch(language) {
-        case "pt-br":
-            $( ".EN" ).addClass( "d-none" );
-            break;
-        default:
-            $( ".PT-BR" ).addClass( "d-none" );
-    }
+
+    // changes text to the desired language
+    $( ".text" ).each(function() {
+        var location = $(this).html().split(':');
+        switch(language) {
+            case "pt-br":
+                $(this).html(PT_BR[location[0]][location[1]]);
+                break;
+            default:
+                $(this).html(EN[location[0]][location[1]]);
+        }
+    });
 });
