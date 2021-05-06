a2e29aa351c3b6d833919b46f316d2b2195906b8
Julio Cesar <60150667+jvneto@users.noreply.github.com>
2021-05-06 08:48:29 -0300

    v1.3.2

--- scripts/main.js
+++ scripts/main.js
@@ -3,9 +3,9 @@ $(document).ready(function() {
         $(".hamburger-menu").toggleClass("pressed");
         $(".side-navigation").toggleClass("hidden");
     });
+
     var $doc = $('html, body');
     $('a').click(function() {
-        console.log("Aqui Scrooll")
         $doc.animate({
             scrollTop: $($.attr(this, 'href')).offset().top - 100
         }, 500);
